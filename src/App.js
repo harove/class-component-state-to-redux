e69381ecfa836806787a158946d2efc9bfc7119d
edgar <harove@gmail.com>
2019-11-26 12:55:09 -0300

    refactored directory structure as different folders for store, reducers and actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
-import {createStore} from 'redux';
-import reducer from './reducers/index'
+import {store} from './store'
 import './App.css';
 import HalloWelt from './components/hallo-welt/HalloWelt';
 
 
-const initialState = {tech: 'React'}
-const store = createStore(reducer,initialState);
+
 
 class App extends Component{
     render() {
